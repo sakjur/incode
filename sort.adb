09156135357b914c73d6723a69da3a3435c164c7
Emil Tullstedt <emiltu@kth.se>
2014-08-19 19:26:20 +0200

    Added Ada

---
+++ sort.adb
@@ -0,0 +1,45 @@
+with Ada.Text_IO;
+
+procedure sort is
+    pragma Suppress (all_checks);
+    package IO renames Ada.Text_IO;
+    type Number_List is array (Positive range <>) of Integer;
+    data : Number_List := (0, 42, 256, 4, 4711, -1337);
+
+    function Fn_part(Offset : In Integer; Last : In Integer) return Integer is
+        Index : Integer := Offset;
+        temporary : Integer;
+    begin
+        temporary := data(Last);
+        data(Last) := data(Index);
+        data(Index) := temporary;
+        for I in Integer range Index .. Last loop
+            if data(I) < data(Last) then
+                temporary := data(I);
+                data(I) := data(Index);
+                data(Index) := temporary;
+                Index := Index + 1;
+            end if;
+        end loop;
+        temporary := data(Last);
+        data(Last) := data(Index);
+        data(Index) := temporary;
+        return Index;
+    end Fn_part;
+
+    procedure Fn (Offset : In Integer; Last : In Integer) is
+        Pi : Integer;
+    begin
+        if Offset < Last then
+            Pi := Fn_part(Offset, Last);
+            Fn(Offset, Pi-1);
+            Fn(Pi+1, Last);
+        end if;
+    end Fn;
+
+begin
+    Fn(1, data'length);
+    for I in Integer range 1 .. data'length loop
+        IO.Put_Line(Integer'Image(data(I)));
+    end loop;
+end sort;
