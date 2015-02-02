c8a445573deeef3be181cc6ee59533ac4e7854ed
Emil Tullstedt <sakjur@gmail.com>
2015-02-02 15:17:57 +0100

    Updated fibonacci for Erlang and added C

--- fib.erl
+++ fib.erl
@@ -3,5 +3,9 @@
 -export([fib/1]).
 
 fib(N) when N == 1; N =:= 0 -> 1;
-fib(N) -> fib(N-1) + fib(N-2).
+fib(N) -> fib(N-2, 1, 1).
+
+fib(0, Prev, Prevprev) -> Prev + Prevprev;
+fib(N, Prev, Prevprev) ->
+    fib(N-1, Prev+Prevprev, Prev).
 
