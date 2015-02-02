89092ba058e3f0f7f4a9f347640309d3cd1453b6
Emil Tullstedt <sakjur@gmail.com>
2015-02-02 18:14:15 +0100

    Updated Haskell to support LARGE digits

--- fib.hs
+++ fib.hs
@@ -1,10 +1,10 @@
-fib :: Int -> Int 
+fib :: Int -> Integer
 fib 0 =
     1
 fib n =
     fib' n 1 0
 
-fib' :: Int -> Int -> Int -> Int
+fib' :: Int -> Integer -> Integer -> Integer
 fib' 1 prev prevprev =
     prev+prevprev
 fib' n prev prevprev =
