c766665eff9899467b3e6056a94c34a821411e41
Emil Tullstedt <sakjur@gmail.com>
2015-03-27 21:24:38 +0100

    Prettified erlang code

--- prime.erl
+++ prime.erl
@@ -6,15 +6,15 @@ map(N) when N < 2 ->
 map(N) when N rem 2 =:= 0 ->
     map(N-1);
 map(N) ->
-    List = gen_list(N, 2),
+    List = gen_list(N),
     reduce([2], List, N).
 
-gen_list(N, _) when N < 3 ->
+gen_list(N) when N < 3 ->
     [];
-gen_list(N, Steps) ->
-    gen_list(N-Steps, Steps) ++ [N].
+gen_list(N) ->
+    gen_list(N-2) ++ [N].
 
-reduce(P, [], N) ->
+reduce(P, [], _) ->
     P;
 reduce(P, [H | T], N) ->
     SqrH = math:pow(H, 2),
