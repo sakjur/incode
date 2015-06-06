1e910a7dd8e01ef3b11edcb06d1b98b2796d3a47
Emil Tullstedt <emiltu@kth.se>
2015-06-06 18:49:42 +0200

    Added perl

---
+++ prime.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+sub IsPrime {
+    (1x$_[0]) =~ /^1?$|^(11+?)\1+$/ || return $_[0]; # wtf?
+    return 0;
+}
+
+my $i = 1;
+print("[");
+for ($i = 1; $i <= 100; $i++) {
+    if (IsPrime($i)) {
+        print(" $i ");
+    }
+}
+print("]\n");
