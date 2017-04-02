26ee683f0645dd6be6742772d2ab32d7c5bcbf90
Emil Tullstedt <sakjur@gmail.com>
2017-04-03 01:23:40 +0200

    Improved Rust

--- logsize/decibyte.rs
+++ logsize/decibyte.rs
@@ -8,18 +8,32 @@ fn decibyte(bytes: f64) -> f64 {
 
 fn main() {
     let iter = env::args().skip(1).map(|f| (fs::metadata(&f), f));
+    let mut total = 0.0_f64;
 
     for file in iter {
         let metadata = file.0;
         let name = file.1;
         match metadata {
             Ok(meta) => {
-                println!("{:9.2}dBy {}", decibyte(meta.len() as f64), name)
+                let size = meta.len() as f64;
+                total = total + size;
+                println!("{:9.2}dBy {}", decibyte(size), name)
             },
             Err(_) => {
                 println!("File not found: {}", name);
                 process::exit(1)
             }
         }
+    }
+
+    if env::args().count() == 1 {
+        let exec: String = match env::args().next() {
+            Some(e) => e,
+            None => "decibyte".to_owned() // Should never be used 
+        };
+        println!("Usage: {} <file> [file1 file2 ...]", exec)
+    }
+    if env::args().count() > 2 {
+        println!("{:9.2}dBy == TOTAL ==", decibyte(total));
     }
 }
